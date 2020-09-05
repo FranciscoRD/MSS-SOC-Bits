1819b3c4de63fa0a122f13aab1c493d05ddbfc33
FranciscoRD <francisco_renan-dt@hotmail.com>
2020-09-04 20:59:14 -0600

    Update google-test.py

--- google-test.py
+++ google-test.py
@@ -1,7 +1,26 @@
+#!/usr/bin/env python
 import os
 
 termino = 'loana+lopez+gonzales'
 ubicacion = 'linkedin.com'
+
 buscador = f'http://www.google.com/search?q=intext:{termino}+inurl:{ubicacion}'
+
+#Buscar en windows
 busqueda = f'start {buscador}'
+
+#Buscar en Linux
+# busqueda = f'x-www-browser {buscador}'
+
+#Buscar en Linux
+# busqueda = f'xdg-open {buscador}'
+
+#Buscar en Linux
+# busqueda = f'gnome-open {buscador}'
+
+#Si lynx esta instalado
+#busqueda = f'lynx {buscador}'
+#Buscar y guradar el resultado
+#busqueda = f'lynx -source {buscador} > prueba.html'
+
 os.system(busqueda)
