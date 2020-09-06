5751c95c32533c3ab866f7817d26eddabe679b6f
FranciscoRD <francisco_renan-dt@hotmail.com>
2020-09-06 10:03:03 -0600

    Update phishtank-parser.py

--- phishtank-parser.py
+++ phishtank-parser.py
@@ -7,20 +7,20 @@ def Procesar():
     elementos = [] 
 
     with open(archivo, 'r') as csvfile: 
-	    csvreader = csv.reader(csvfile) 
-	    campos = next(csvreader) 
-	    for row in csvreader:
-    		elementos.append(row)
-	    print("Numero total de registros: %d"%(csvreader.line_num))
+        csvreader = csv.reader(csvfile) 
+        campos = next(csvreader) 
+        for row in csvreader:
+            elementos.append(row)
+        print("Numero total de registros: %d"%(csvreader.line_num))
 
     print('Nombre de Campos:' + ', '.join(campo for campo in campos)) 
 
     mostrar = int(input(f'Cuantos registros quiere ver ( 1 - {csvreader.line_num}) ?: '))
-    print(f'\nRegistro {mostrar}:\n') 
-    for row in elementos[:mostrar]: 
-	    for col in row: 
-		    print(col), 
-	    print('\n')
+    for row in elementos[:mostrar]:
+        print("="*20)
+        for col in row: 
+            print(col), 
+    print('\n')
 
 
 if os.path.isfile('online-valid.csv'):
